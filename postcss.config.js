9ba32f21c0099eac6516cd4400d5e3ce3468540c
Vladimir Alabov <vladimir.alabov@bsc-ideas.com>
2019-11-14 17:02:49 +0300

    Tailwind

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+// only needed if you want to purge
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+// 	content: ["./src/**/*.svelte", "./public/**/*.html"],
+// 	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+// });
+
+module.exports = {
+	plugins: [
+		require('postcss-import'),
+		require("tailwindcss"),
+		require('postcss-nested'),
+		require('postcss-custom-properties'),
+		require('autoprefixer')
+		// only needed if you want to purge
+		// ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+	]
+};
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let name;
+</script>
+
+<div class="shadow">
+	<h1>
+		{name}!
+	</h1>
+
+	<div class="button">
+		Kek
+	</div>
+</div>
+
+<style>
+	.shadow {
+		@apply shadow;
+	}
+	h1 {
+		@apply font-bold py-2 px-4 rounded;
+	}
+	.button {
+		@apply font-bold py-2 px-4 rounded bg-blue-500 text-white transition-bg cursor-pointer;
+	}
+	.button:hover {
+		@apply bg-blue-700;
+	}
+</style>
