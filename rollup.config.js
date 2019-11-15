b3ea4eec20ef0875cf94f1f37cad54d29cc5ad28
Vladimir Alabov <vladimir.alabov@bsc-ideas.com>
2019-11-15 15:56:19 +0300

    Clean rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -10,42 +10,28 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
   input: 'src/main.js',
   output: {
-    sourcemap: true,
+    sourcemap: !production,
     format: 'iife',
     name: 'app',
     file: 'public/bundle.js'
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
       dev: !production,
-
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
       css: css => {
         css.write('public/bundle.pcss');
       },
-
       preprocess: sveltePreprocess({postcss: true})
     }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
     resolve({
       browser: true,
       dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
     }),
     commonjs(),
 
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
     !production && livereload('public'),
 
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
     production && terser()
   ],
   watch: {
